1f5900c88c7ca45420deb922a9a88639f0764d7a
Ludwig Walther <lcwalther@gmail.com>
2023-01-23 11:52:56 +0100

    feat: make search results only render on search

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import './App.css';
 import { SearchButton } from './components/SearchButton/';
 import { SearchResults } from './components/SearchResults/';
 
 function App() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['searchData'],
-    queryFn: () =>
-      fetch(
-        'https://cdn.statcdn.com/static/application/search_results.json'
-      ).then((res) => res.json())
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { data } = useQuery({
+    queryKey: ['searchData', searchTerm],
+    queryFn: () => {
+      if (searchTerm.toLowerCase() === 'statista') {
+        return fetch(
+          'https://cdn.statcdn.com/static/application/search_results.json'
+        ).then((res) => res.json());
+      } else {
+        return null;
+      }
+    }
   });
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      search: { value: string };
+    };
+    setSearchTerm(target.search.value);
+  };
 
   return (
     <div className="App h-screen">
@@ -21,17 +37,21 @@ function App() {
         <h2 className="text-base md:text-2xl text-statista-dark2">
           Insights und Fakten aus 170 Branchen und 150+ Ländern
         </h2>
-        <form className="mt-12 md:mt-16 max-w-screen-md w-full flex">
+        <form
+          className="mt-12 md:mt-16 max-w-screen-md w-full flex"
+          onSubmit={handleSubmit}
+        >
           <input
             className="placeholder:italic h-14 pl-6 pr-20 w-full rounded-full focus:outline focus:outline-4"
             placeholder="Statistiken, Prognosen und Umfragen finden"
             type="search"
+            name="search"
           ></input>
           <SearchButton />
         </form>
       </section>
       <section className="max-w-screen-md mx-auto px-5 py-8">
-        <SearchResults isLoading={isLoading} items={data?.items} />
+        <SearchResults items={data?.items} />
       </section>
     </div>
   );
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -3,20 +3,16 @@ import { TSearchResult } from '../../types';
 import { SearchResultItem } from '../SearchResultItem';
 
 interface ISearchResultsProps {
-  isLoading: boolean;
   items: TSearchResult[];
 }
 
 export const SearchResults: FC<ISearchResultsProps> = ({
-  isLoading,
   items
 }: ISearchResultsProps) => {
   return (
     <>
-      {isLoading ? (
-        <div className="text-statista-grey text-base">
-          Suchergebnisse werden geladen
-        </div>
+      {!items ? (
+        <div className="text-statista-grey text-base">Keine Suchergebnisse</div>
       ) : (
         items?.map((item: TSearchResult) => (
           <SearchResultItem key={item.identifier} item={item} />
