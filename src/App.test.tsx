346f87538c87b392033444f0dd92c5c7a11e47c9
Ludwig Walther <lcwalther@gmail.com>
2023-01-25 15:03:32 +0100

    chore: extract assertions from render function instead of screen object

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,76 +1,98 @@
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import { screen } from '@testing-library/react';
 import App from './App';
 import { customRender } from './test-data/';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import * as hooks from './routes/hooks';
+import { UseQueryResult } from '@tanstack/react-query';
 
 describe('App', () => {
   it('renders the top navigation', async () => {
     // given
-    const { user } = customRender(
+    const { user, getByText, getByRole } = customRender(
       <BrowserRouter>
         <App />
       </BrowserRouter>
     );
 
-    expect(screen.getByText('Suche')).toBeInTheDocument();
-    expect(screen.getByText('Favoriten')).toBeInTheDocument();
+    expect(getByText('Suche')).toBeInTheDocument();
+    expect(getByText('Favoriten')).toBeInTheDocument();
 
     // when
-    await user.click(screen.getByText('Favoriten'));
+    await user.click(getByText('Favoriten'));
 
     // then
-    const titleValue = screen.getByRole('heading', { name: 'Favoriten' });
+    const titleValue = getByRole('heading', { name: 'Favoriten' });
     expect(titleValue).toBeInTheDocument();
   });
 
   it('renders the search page at root path', async () => {
-    customRender(
+    // given
+    // when
+    const { getByRole } = customRender(
       <MemoryRouter initialEntries={['/']}>
         <App />
       </MemoryRouter>
     );
 
-    const titleValue = screen.getByRole('heading', {
+    const titleValue = getByRole('heading', {
       name: 'Empowering people with data'
     });
+
+    // then
     expect(titleValue).toBeInTheDocument();
   });
 
   it('renders the search page at path /search', async () => {
-    customRender(
+    // given
+    // when
+    const { getByRole } = customRender(
       <MemoryRouter initialEntries={['/search']}>
         <App />
       </MemoryRouter>
     );
 
-    const titleValue = screen.getByRole('heading', {
+    const titleValue = getByRole('heading', {
       name: 'Empowering people with data'
     });
+
+    // then
     expect(titleValue).toBeInTheDocument();
   });
 
   test.skip('renders the detail page at path /items/:id', async () => {
-    customRender(
+    // given
+    const useFetchDetailsSpy = vi
+      .spyOn(hooks, 'useFetchDetails')
+      .mockImplementation(() => {
+        return Promise.resolve({
+          data: { items: [{ identifier: 1, title: 'item 1' }] }
+        } as UseQueryResult<any, unknown>);
+      });
+
+    // when
+    const { getByText } = customRender(
       <MemoryRouter initialEntries={['/items/661751']}>
         <App />
       </MemoryRouter>
     );
+    const titleValue = getByText('item 1');
 
-    const titleValue = screen.getByRole('heading', {
-      name: 'Gesundheit - Bekanntheit von Krankenkassen in Deutschland 2017'
-    });
+    // then
     expect(titleValue).toBeInTheDocument();
   });
 
   it('renders the favorites page at path /favorites', async () => {
-    customRender(
+    // given
+
+    // when
+    const { getByRole } = customRender(
       <MemoryRouter initialEntries={['/favorites']}>
         <App />
       </MemoryRouter>
     );
 
-    const titleValue = screen.getByRole('heading', { name: 'Favoriten' });
+    const titleValue = getByRole('heading', { name: 'Favoriten' });
     expect(titleValue).toBeInTheDocument();
   });
 });
